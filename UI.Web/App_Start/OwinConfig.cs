20c7fa6b112efce4260414881a3e759d917cffae
Dmitry Nogin <dmitryn@mail.com>
2015-09-30 23:39:36 -0500

    Add Identity to Infrastructure.

---
+++ UI.Web/App_Start/OwinConfig.cs
@@ -0,0 +1,27 @@
+﻿using Events;
+using Microsoft.AspNet.Identity;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Web.App_Start
+{
+        public class OwinConfig
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                app.ExecuteAsync().Wait();
+
+                app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+                //app.UseFacebookAuthentication(new FacebookAuthenticationOptions
+                //{
+                //    AppId = FacebookConfigurationSection.Recruiting.AppId,
+                //    AppSecret = FacebookConfigurationSection.Recruiting.AppSecret,
+                //    Provider = new FacebookAuthProvider()
+                //});
+            }
+    }
+}
